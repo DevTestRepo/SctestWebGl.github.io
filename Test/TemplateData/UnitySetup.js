39eb55a24346c6f53f0d26a6f7f1db613777389a
fiveDVR <mohamed.helmy@5d-vr.com>
2023-02-15 17:11:14 +0200

    .

--- Test/TemplateData/UnitySetup.js
+++ Test/TemplateData/UnitySetup.js
@@ -50,4 +50,17 @@ script.onload = () => {
             alert(message);
         });
 };
+var CopyPastePlugin =
+{
+    CopyPasteReader: function (gObj, vName) {
+        var gameObjectName = UTF8ToString(gObj);
+        var voidName = UTF8ToString(vName);
+        navigator.clipboard.readText().then(function (data) {
+            gameInstance.SendMessage(gameObjectName, voidName, data);
+        }, function () {
+            gameInstance.SendMessage(gameObjectName, voidName, "no text aviable in clipboard");
+        })
+    }
+};
+mergeInto(LibraryManager.library, CopyPastePlugin);
 document.body.appendChild(script);
