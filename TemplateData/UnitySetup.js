98e0b6baad610994cf1dbe0a715ede6cb13de7bd
fiveDVR <mohamed.helmy@5d-vr.com>
2023-03-13 13:24:59 +0200

    Update UnitySetup.js

--- TemplateData/UnitySetup.js
+++ TemplateData/UnitySetup.js
@@ -40,7 +40,7 @@ script.src = loaderUrl;
 script.onload = () => {
     createUnityInstance(canvas, config, (progress) => {
        progressBarFull.style.width = 100 * progress + "%";
-       console.log(progress);
+       console.log("progreeeeeeeeeeees"+progress);
     })
         .then((unityInstance) => {
             unityGame = unityInstance;
